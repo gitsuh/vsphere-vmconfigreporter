e6817d950db5228feeec9ac1a1ea5cda52c00575
good-paste <good-paste@users.noreply.github.com>
2017-07-08 09:09:18 -0400

    Added disk allocation sum and removed debugging

--- report-vmconfig.ps1
+++ report-vmconfig.ps1
@@ -1,5 +1,6 @@
 $vms = get-view -viewtype VirtualMachine
 $vmfolders = get-folder -type VM
+$resourcepools = get-resourcepool
 $arrayofvms = @()
 foreach ($vm in $vms){
 	$harddiskcounter = 0
@@ -16,11 +17,9 @@ foreach ($vm in $vms){
 	$tempobj | add-member -name "Modified" -membertype NoteProperty -value $vm.config.Modified
 	$tempobj | add-member -name "isTemplate" -membertype NoteProperty -value $vm.config.Template
 	$tempobj | add-member -name "GuestID" -membertype NoteProperty -value $vm.config.GuestId
-#	$tempobj | add-member -name "DatastoreURL" -membertype NoteProperty -value $([string]::join(",",$vm.config.datastoreurl))
 	$tempobj | add-member -name "MemoryHotAddEnabled" -membertype NoteProperty -value $vm.config.MemoryHotAddEnabled
 	$tempobj | add-member -name "CPUHotAddEnabled" -membertype NoteProperty -value $vm.config.CPUHotAddEnabled
 	$tempobj | add-member -name "CPUHotRemoveEnabled" -membertype NoteProperty -value $vm.config.CPUHotRemoveEnabled
-	$tempobj | add-member -name "VMX" -membertype NoteProperty -value $([string]::join(",",$vm.config.files))
 	$tempobj | add-member -name "VMXFilepath" -membertype NoteProperty -value $vm.summary.config.vmpathname
 	$tempobj | add-member -name "toolsversion" -membertype NoteProperty -value $vm.config.tools.toolsversion
 	$tempobj | add-member -name "vcpu" -membertype NoteProperty -value $vm.config.hardware.numcpu
@@ -35,6 +34,12 @@ foreach ($vm in $vms){
 			$tempobj | add-member -name "vmfolder" -membertype NoteProperty -value $folder.name
 		}
 	}
+	foreach ($resourcepool in $resourcepools){
+		if($vm.resourcepool -eq $resourcepool.id){
+			$tempobj | add-member -name "resourcepool" -membertype NoteProperty -value $resourcepool.name
+			$tempobj | add-member -name "resourcepoolparent" -membertype NoteProperty -value $resourcepool.parent
+		}
+	}
 	$tempdevices = $vm.Config.hardware.device
 	$tempdisklayout = $vm.layout.disk
 	$tempguestnet = $vm.guest.net
@@ -42,10 +47,7 @@ foreach ($vm in $vms){
 		foreach ($device in $tempdevices){
 			if($device.deviceinfo.label -like "Net*"){
 				if ($device.key -eq $net.deviceconfigid){
-					#read-host "startif"
 					$niccounter ++
-					#write-host "NIC counter" $niccounter
-					#read-host "preadd"
 					if($net.ipaddress -ne $null){
 					$tempobj | add-member -name "nicipaddress$niccounter" -membertype NoteProperty -value $([string]::join(",",$net.ipaddress))
 					}else{	
@@ -53,13 +55,7 @@ foreach ($vm in $vms){
 					}
 					$tempobj | add-member -name "nic$niccounter" -membertype NoteProperty -value $device.deviceinfo.label
 					$tempobj | add-member -name "nicmac$niccounter" -membertype NoteProperty -value $device.macaddress
-					#$([string]::join(",",$device.macaddress))
-					#write-host $device.label
-					#read-host "dev"
 					$tempobj | add-member -name "nicsportgroup$niccounter" -membertype NoteProperty -value $device.deviceinfo.summary
-					#write-host $device.summary
-					#read-host "last"
-					#$tempobj | out-gridview
 				}
 			}
 		}
@@ -67,29 +63,26 @@ foreach ($vm in $vms){
 	foreach ($device in $tempdevices){
 		if($device.deviceinfo.label -like "SCSI*"){
 			$scsicounter ++
-			#write-host "SCSI counter" $scsicounter
 			$tempobj | add-member -name "scsisharing$scsicounter" -membertype noteproperty -value $device.SharedBus
 			$tempobj | add-member -name "scsi$scsicounter"-membertype NoteProperty -value $device.deviceinfo.label
 			$tempobj | add-member -name "scsisize$scsicounter" -membertype NoteProperty -value $device.deviceinfo.summary
 		}
 	}
+	$disksum = 0
 	foreach ($diskid in $tempdisklayout){
 		foreach ($device in $tempdevices){
 			if($device.deviceinfo.label -like "Hard*"){
-				#write-host "Hard disk counter" $harddiskcounter
-				#write-host "Hard disk device key" $device.key
-				#write-host "Hard disk disk id key" $diskid.key
 				if($device.key -eq $diskid.key){
 				$harddiskcounter ++
-				#write-host ""
 					$tempobj | add-member -name "harddisklocation$harddiskcounter" -membertype NoteProperty -value $([string]::join(",",$diskid.diskfile))
 					$tempobj | add-member -name "harddisk$harddiskcounter" -membertype NoteProperty -value $device.deviceinfo.label
 					$tempobj | add-member -name "harddisksize$harddiskcounter" -membertype NoteProperty -value $device.deviceinfo.summary
+						$disksum = $disksum + [int]$($($device.deviceinfo.summary -replace '[^0-9]')/1024/1024)
 				}
 			}
 		}
-	}	
-	#$tempobj | add-member -name "devicesummary" -membertype NoteProperty -value $vm.Config.hardware.device.deviceinfo
-	$arrayofvms += $tempobj
 	}
+	$tempobj | add-member -name "diskprovisionednosnapshotsGB" -membertype NoteProperty -value $disksum
+	$arrayofvms += $tempobj
+}
 $arrayofvms | export-csv report.csv -notype
