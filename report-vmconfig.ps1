d481031048d5367e5a3099aca7f92ff5e2e1a5d0
good-paste <good-paste@users.noreply.github.com>
2017-07-08 07:48:35 -0400

    added additional reporting fixed bugs
    added folder name using matching object ids
    added export-csv feature instead of out-gridview
    fixed System.String[] issue with export-csv

--- report-vmconfig.ps1
+++ report-vmconfig.ps1
@@ -1,4 +1,5 @@
 $vms = get-view -viewtype VirtualMachine
+$vmfolders = get-folder -type VM
 $arrayofvms = @()
 foreach ($vm in $vms){
 	$harddiskcounter = 0
@@ -6,6 +7,7 @@ foreach ($vm in $vms){
 	$scsicounter = 0
 	$tempobj = New-Object System.Object
 	$tempobj | add-member -name "Name" -membertype NoteProperty -value $vm.Name
+	$tempobj | add-member -name "SDK" -membertype NoteProperty -value $vm.client.serviceurl
 	$tempobj | add-member -name "hostname" -membertype NoteProperty -value $vm.guest.hostname
 	$tempobj | add-member -name "moref" -membertype NoteProperty -value $vm.moref
 	$tempobj | add-member -name "Uuid" -membertype NoteProperty -value $vm.config.Uuid
@@ -14,15 +16,25 @@ foreach ($vm in $vms){
 	$tempobj | add-member -name "Modified" -membertype NoteProperty -value $vm.config.Modified
 	$tempobj | add-member -name "isTemplate" -membertype NoteProperty -value $vm.config.Template
 	$tempobj | add-member -name "GuestID" -membertype NoteProperty -value $vm.config.GuestId
-	$tempobj | add-member -name "DatastoreURL" -membertype NoteProperty -value $vm.config.datastoreurl
+#	$tempobj | add-member -name "DatastoreURL" -membertype NoteProperty -value $([string]::join(",",$vm.config.datastoreurl))
 	$tempobj | add-member -name "MemoryHotAddEnabled" -membertype NoteProperty -value $vm.config.MemoryHotAddEnabled
 	$tempobj | add-member -name "CPUHotAddEnabled" -membertype NoteProperty -value $vm.config.CPUHotAddEnabled
 	$tempobj | add-member -name "CPUHotRemoveEnabled" -membertype NoteProperty -value $vm.config.CPUHotRemoveEnabled
-	$tempobj | add-member -name "VMX" -membertype NoteProperty -value $vm.config.files
+	$tempobj | add-member -name "VMX" -membertype NoteProperty -value $([string]::join(",",$vm.config.files))
+	$tempobj | add-member -name "VMXFilepath" -membertype NoteProperty -value $vm.summary.config.vmpathname
 	$tempobj | add-member -name "toolsversion" -membertype NoteProperty -value $vm.config.tools.toolsversion
 	$tempobj | add-member -name "vcpu" -membertype NoteProperty -value $vm.config.hardware.numcpu
 	$tempobj | add-member -name "corepersocket" -membertype NoteProperty -value $vm.config.hardware.numcorespersocket
 	$tempobj | add-member -name "memorymb" -membertype NoteProperty -value $vm.config.hardware.memorymb
+	$tempobj | add-member -name "powerstate" -membertype NoteProperty -value $vm.summary.runtime.PowerState
+	$tempobj | add-member -name "connectionstate" -membertype NoteProperty -value $vm.summary.runtime.connectionstate
+	$tempobj | add-member -name "boottime" -membertype NoteProperty -value $vm.summary.runtime.boottime
+	$tempobj | add-member -name "consolidationneeded" -membertype NoteProperty -value $vm.summary.runtime.consolidationneeded
+	foreach ($folder in $vmfolders){
+		if($vm.parent -eq $folder.id){
+			$tempobj | add-member -name "vmfolder" -membertype NoteProperty -value $folder.name
+		}
+	}
 	$tempdevices = $vm.Config.hardware.device
 	$tempdisklayout = $vm.layout.disk
 	$tempguestnet = $vm.guest.net
@@ -34,9 +46,14 @@ foreach ($vm in $vms){
 					$niccounter ++
 					#write-host "NIC counter" $niccounter
 					#read-host "preadd"
-					$tempobj | add-member -name "nicipaddress$niccounter" -membertype NoteProperty -value $net.ipaddress
+					if($net.ipaddress -ne $null){
+					$tempobj | add-member -name "nicipaddress$niccounter" -membertype NoteProperty -value $([string]::join(",",$net.ipaddress))
+					}else{	
+						$tempobj | add-member -name "nicipaddress$niccounter" -membertype NoteProperty -value "null"
+					}
 					$tempobj | add-member -name "nic$niccounter" -membertype NoteProperty -value $device.deviceinfo.label
 					$tempobj | add-member -name "nicmac$niccounter" -membertype NoteProperty -value $device.macaddress
+					#$([string]::join(",",$device.macaddress))
 					#write-host $device.label
 					#read-host "dev"
 					$tempobj | add-member -name "nicsportgroup$niccounter" -membertype NoteProperty -value $device.deviceinfo.summary
@@ -65,7 +82,7 @@ foreach ($vm in $vms){
 				if($device.key -eq $diskid.key){
 				$harddiskcounter ++
 				#write-host ""
-					$tempobj | add-member -name "harddisklocation$harddiskcounter" -membertype NoteProperty -value $diskid.diskfile
+					$tempobj | add-member -name "harddisklocation$harddiskcounter" -membertype NoteProperty -value $([string]::join(",",$diskid.diskfile))
 					$tempobj | add-member -name "harddisk$harddiskcounter" -membertype NoteProperty -value $device.deviceinfo.label
 					$tempobj | add-member -name "harddisksize$harddiskcounter" -membertype NoteProperty -value $device.deviceinfo.summary
 				}
@@ -75,4 +92,4 @@ foreach ($vm in $vms){
 	#$tempobj | add-member -name "devicesummary" -membertype NoteProperty -value $vm.Config.hardware.device.deviceinfo
 	$arrayofvms += $tempobj
 	}
-$arrayofvms | out-gridview
+$arrayofvms | export-csv report.csv -notype
